72e1ec597ba825d708f419f26bf5f6952739d354
valb-mig <valb.mig@gmail.com>
2026-09-22 14:17:14 -0300

    chore(app): sobe versão para 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.valb.copyguru"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     signingConfigs {
